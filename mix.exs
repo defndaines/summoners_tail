77c27bbdffa2fea4dfb741c6a720918669e0cf99
Michael Daines <953037+defndaines@users.noreply.github.com>
2022-02-26 07:00:20 -0800

    Add dependencies
    Will need JSON library for parsing responses. Credo and Test Watch are
    for developer convenience. `:inets` and `:ssl` are to use the
    OTP-provided `:httpc` library for making remote calls.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule SummonersTail.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :inets, :ssl]
     ]
   end
 
@@ -23,6 +23,9 @@ defmodule SummonersTail.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:jason, "~> 1.3"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
