4b58a30e531b0f148cd646fb5144294981294229
Michael Daines <953037+defndaines@users.noreply.github.com>
2022-02-26 16:25:43 -0800

    recent_summoners function to aggregate calls
    Combine all the calls built up to this point, chaining them to assemble
    a list of names of all summoners the provided summoner has played with
    in the last five matches.
    
    When collecting the match information, parallelized into five separate
    tasks to speed up the response, since they are each independent.

--- lib/summoners_tail.ex
+++ lib/summoners_tail.ex
@@ -3,16 +3,41 @@ defmodule SummonersTail do
   Documentation for `SummonersTail`.
   """
 
+  alias RiotAPI.HTTPC.Match
+  alias RiotAPI.HTTPC.Summoner
+  alias RiotAPI.Kit
+
+  require Logger
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SummonersTail.hello()
-      :world
-
+  Get a list of recent summoners (by name) a given summoner has played with.
   """
-  def hello do
-    :world
+  def recent_summoners(summoner_name, region) do
+    with {:ok, summoner} <- Summoner.by_name(summoner_name, region),
+         puuid <- Kit.summoner_puuid(summoner),
+         {:ok, match_region} <- Kit.summoner_region_to_match_region(region),
+         {:ok, match_ids} <- Match.recent(puuid, match_region) do
+      {_summoner_name, participants} =
+        match_participants(match_ids, match_region)
+        |> Map.pop(puuid)
+
+      Map.values(participants)
+    end
+  end
+
+  # Grab the MatchDTO for each match ID and extract the participants.
+  defp match_participants(match_ids, match_region) do
+    match_ids
+    |> Enum.map(fn match_id -> Task.async(fn -> Match.info(match_id, match_region) end) end)
+    |> Enum.reduce(%{}, fn task, acc ->
+      case Task.await(task) do
+        {:ok, match} ->
+          match |> Kit.match_participant_map() |> Map.merge(acc)
+
+        error ->
+          Logger.error("Problem retrieving match", error)
+          acc
+      end
+    end)
   end
 end
--- test/summoners_tail_test.exs
+++ test/summoners_tail_test.exs
@@ -1,8 +1,4 @@
 defmodule SummonersTailTest do
   use ExUnit.Case
-  doctest SummonersTail
 
-  test "greets the world" do
-    assert SummonersTail.hello() == :world
-  end
 end
